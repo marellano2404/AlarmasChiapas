c6ea696bc6133d1592569aa2db1159e3100f877a
ISC. Marco Antonio <maarellano@live.com>
2022-01-25 09:02:05 -0600

    Repottes

---
+++ AlarmasWPF/Reportes/ReportesUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlarmasWPF.Reportes
+{
+    /// <summary>
+    /// Lógica de interacción para ReportesUC.xaml
+    /// </summary>
+    public partial class ReportesUC : UserControl
+    {
+        public ReportesUC()
+        {
+            InitializeComponent();
+        }
+
+        private void Buscar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Regresar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
